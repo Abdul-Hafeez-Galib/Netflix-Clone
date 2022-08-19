d81dc213fc1beae9ac0c48e84f876e4580a4ee89
Abdul-Hafeez-Galib <galibabdulhafeez@gmail.com>
2022-08-19 15:00:12 +0530

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "f2be02e3537e90c67f66618541d104f0";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
